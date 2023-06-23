8604fea7acdd519f9cabf51b6d9d008651f39f53
JaredJohnson1995 <137435351+JaredJohnson1995@users.noreply.github.com>
2023-06-22 19:41:48 -0500

    Add files via upload
    Updated the script to include a test for the application 90s after deployment

--- EKS-Terraform/exercisescript.ps1
+++ EKS-Terraform/exercisescript.ps1
@@ -7,7 +7,29 @@ kubectl create secret docker-registry regcred `
   --docker-username=AWS `
   --docker-password=$(aws ecr get-login-password) `
   --namespace=health-check
-  kubectl apply -f manifest.yaml -n health-check
-  kubectl get svc -n health-check
-  echo please give the cluster about 1 minute 30 seconds to provision the load balancer/external IP. Then copy paste the external IP with :8000 at the end
-  echo the result should be ......elb.amazonaws.com:8000
+kubectl apply -f manifest.yaml -n health-check
+start-sleep -seconds 20
+kubectl get svc -n health-check
+echo "please give the cluster about 1 minute to provision the load balancer/external IP. Then copy paste the external IP with :8000 at the end"
+echo "the result should be ......elb.amazonaws.com:8000"
+start-sleep -seconds 70
+$output = kubectl get svc -n health-check
+
+$elbAddress = $output | Select-String -Pattern '[a-zA-Z0-9\-]+\.us-east-1\.elb\.amazonaws\.com' | ForEach-Object { $_.Matches.Value }
+
+$elbAddress = $elbAddress -replace '\r\n', ''
+
+$hostname = $elbAddress -replace ':\d+', ''
+
+$uri = [System.UriBuilder]::new()
+$uri.Scheme = "http"
+$uri.Host = $hostname
+$uri.Port = 8000
+
+$request = [System.Net.WebRequest]::Create($uri.Uri)
+$response = $request.GetResponse()
+$responseStream = $response.GetResponseStream()
+$reader = [System.IO.StreamReader]::new($responseStream)
+$responseContent = $reader.ReadToEnd()
+
+$responseContent
